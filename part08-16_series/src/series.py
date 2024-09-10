e7f843461a690172ba233a5639374542836eacbc
Adam Boda <adamboda@Adams-MacBook-Air.local>
2024-09-10 18:05:36 +0200

    latest answers

--- part08-16_series/src/series.py
+++ part08-16_series/src/series.py
@@ -23,30 +23,9 @@ class Series:
 
 
 def minimum_grade(rating: float, series_list: list):
-    filtered_series = filter(lambda x: x.avg_rating >= rating, series_list)
-    print(list(filtered_series))
+    filtered_series = list(filter(lambda x: x.avg_rating >= rating, series_list))
     return filtered_series
 
 def includes_genre(genre: str, series_list: list):
-    filtered_genres = filter(lambda x: genre in x.genres, series_list) 
+    filtered_genres = list(filter(lambda x: genre in x.genres, series_list) )
     return filtered_genres
-
-
-s1 = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
-s1.rate(5)
-
-s2 = Series("South Park", 24, ["Animation", "Comedy"])
-s2.rate(3)
-
-s3 = Series("Friends", 10, ["Romance", "Comedy"])
-s3.rate(2)
-
-series_list = [s1, s2, s3]
-
-print("a minimum grade of 4.5:")
-for series in minimum_grade(4.5, series_list):
-    print(series.title)
-
-print("genre Comedy:")
-for series in includes_genre("Comedy", series_list):
-    print(series.title)
